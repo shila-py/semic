6ae1a7171e7fa6b174ee800d134bc3dc30e3713f
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2021-10-10 12:00:57 -0500

    Added elements.py and changed exp,sqrt from math to numpy

---
+++ src/elements.py
@@ -0,0 +1,13 @@
+##########################################
+# Semiconductor Calculations Package
+# Author: Nithin Kumar Santha Kumar
+# Date: 10/10/2021
+##########################################
+# Changelog: 
+# 4/25/2021 - Started Package
+# 10/10/2021 - Started periodic table of elements (elements.py)
+# 
+##########################################
+
+class Si:
+   
