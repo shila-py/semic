c2d9cea92a319c3b2cefab756e9bfc1d1a685484
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2021-10-28 18:57:47 -0500

    Created quantum mechanics and materials subpackages and moved materials.py into materials subpackage. Created quantum.py under quantum mechanics subpackage

---
+++ src/quantum/quantum.py
@@ -0,0 +1,10 @@
+##########################################
+# Semiconductor Calculations Package
+# Author: Nithin Kumar Santha Kumar
+# Date: 10/10/2021
+##########################################
+# Changelog: 
+# 4/25/2021 - Started Package
+# 10/28/2021 - Started quantum mechanics (quantum.py)
+# 10/28/2021 - Added more functionality
+##########################################
