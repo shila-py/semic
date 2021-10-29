eb9441968659998986eb61d5cbf3079ebd3d1171
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2021-10-28 20:01:11 -0500

    added wavefunction and hermitian operator

--- src/quantum/quantum.py
+++ src/quantum/quantum.py
@@ -8,3 +8,14 @@
 # 10/28/2021 - Started quantum mechanics (quantum.py)
 # 10/28/2021 - Added more functionality
 ##########################################
+
+from sympy.physics.quantum.dagger import Dagger
+from sympy.physics.quantum.state import Bra, Ket
+from sympy.physics.quantum.operator import Operator
+from sympy.physics.quantum import InnerProduct, OuterProduct
+
+def wavefunction():
+   pass
+
+def hermitian():
+   pass
