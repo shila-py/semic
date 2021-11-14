f7a2fb037400f78020e77c78b70d1cbfa318d0bb
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2021-11-13 19:04:37 -0600

    made changes to __init__.py for material classes

--- semic/materials/__init__.py
+++ semic/materials/__init__.py
@@ -1,1 +1,9 @@
-from materials import *
+from .materials import *
+
+__all__ = [
+           'Si','Ge','SiC','AlP','AlAs','AlSb','GaN','GaP','GaAs','GaSb',
+           'InP','InAs','InSb','ZnO','ZnS','ZnSe','ZnTe','CdS','CdSe','CdTe',
+           'HgS','PbS','PbSe','PbTe','SiGe','AlGaAs','AlGaN','AlGaSb','CdMnTe',
+           'GaAsP','HgCdTe','InAlAs','InGaAs','InGaN','AlGaAsSb','GaInAsP'
+           ]
+
