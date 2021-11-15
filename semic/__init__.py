470dfa741b493964c4892dfe89909ea9f75b713f
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2021-11-15 01:50:26 -0600

    fixed errors

--- semic/__init__.py
+++ semic/__init__.py
@@ -1,2 +1,1 @@
-from dynamics import *
-from fundamentals import *
+
--- semic/materials/__init__.py
+++ semic/materials/__init__.py
@@ -1,7 +1,5 @@
-from .materials import *
-
 __all__ = [
-           'Si','Ge','SiC','AlP','AlAs','AlSb','GaN','GaP','GaAs','GaSb',
+           'Semiconductor','Si','Ge','SiC','AlP','AlAs','AlSb','GaN','GaP','GaAs','GaSb',
            'InP','InAs','InSb','ZnO','ZnS','ZnSe','ZnTe','CdS','CdSe','CdTe',
            'HgS','PbS','PbSe','PbTe','SiGe','AlGaAs','AlGaN','AlGaSb','CdMnTe',
            'GaAsP','HgCdTe','InAlAs','InGaAs','InGaN','AlGaAsSb','GaInAsP'
