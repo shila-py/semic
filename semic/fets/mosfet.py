cdb0215a8c4730fa8fa3163873fb3cf9b3cf057e
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2022-06-15 03:47:44 -0500

    updated MOSFET and added new methods

--- semic/fets/mosfet.py
+++ semic/fets/mosfet.py
@@ -212,26 +212,7 @@ class MOSFET:
         t_ox = self.oxide_thickness if t_ox is None else t_ox
 
         return (k_ox * EPSILON_NOUGHT) / t_ox
-    
-    def body_effect_parameter(self,concentration: float,c_ox: float)->float:
-        """_summary_
 
-        Parameters
-        ----------
-        concentration : float
-            _description_
-        c_ox : float
-            _description_
-
-        Returns
-        -------
-        float
-            _description_
-        """
-        ELEMENTARY_CHARGE = value("Elementary charge")
-        gamma = np.sqrt(2*ELEMENTARY_CHARGE*concentration) / c_ox
-        return gamma
-    
     def process_parameter(self,mu: float,c_ox: float)-> float:
         """_summary_
 
@@ -303,10 +284,447 @@ class MOSFET:
 
         return i_dss
 
+    def transconductance(self,
+                         i_d: float=None,
+                         v_gs: float=None,
+                         v_t: float=None,
+                         current_gain: float=None,
+                         process_param: float=None,
+                         width: float=None,
+                         length: float=None)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        i_d : float, optional
+            _description_, by default None
+        v_gs : float, optional
+            _description_, by default None
+        v_t : float, optional
+            _description_, by default None
+        current_gain : float, optional
+            _description_, by default None
+        process_param : float, optional
+            _description_, by default None
+        width : float, optional
+            _description_, by default None
+        length : float, optional
+            _description_, by default None
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        formula_a = [current_gain,v_gs,v_t]
+        formula_b = [process_param,width,length,i_d]
+        formula_c = [i_d,v_gs,v_t]
+        
+        if all(params is not None for params in formula_a):
+            g_m1 = current_gain * (v_gs - v_t)
+            return g_m1
+        elif all(params is not None for params in formula_b):
+            g_m2 = np.sqrt(2 * process_param * (width / length)) * np.sqrt(i_d)
+            return g_m2
+        elif all(params is not None for params in formula_c):
+            g_m3 = 2 * i_d / (v_gs - v_t)
+            return g_m3
+        else:
+            raise TypeError("Function needs 3 correct arguments!")
+
+    def body_transconductance(self,body_effect,transconductance)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        body_effect : _type_
+            _description_
+        transconductance : _type_
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        g_mb = body_effect * transconductance
+        return g_mb
+
+    def body_effect(self,
+                    gamma: float=None,
+                    phi: float=None,
+                    v_sb: float=0.0)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        gamma : float
+            _description_
+        phi : float, optional
+            _description_, by default None
+        v_sb : float, optional
+            _description_, by default 0.0
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        bep = self.body_effect_param if gamma is None else gamma
+        two_phi = self.surface_potential if phi is None else phi
+
+        body_eff = bep * (2 * np.sqrt(two_phi + np.abs(v_sb)))
+
+        return body_eff
+
+    def gate_source_capacitance(self,
+                                c_ox: float,
+                                width: float=None,
+                                length: float=None,
+                                length_ld: float=None,
+                                c_gd: float=None)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        c_ox : float
+            _description_
+        width : float, optional
+            _description_, by default None
+        length : float, optional
+            _description_, by default None
+        length_ld : float, optional
+            _description_, by default None
+
+        Returns
+        -------
+        float
+            _description_
+        """
+
+        l_ov = self.lat_diff_len if length_ld is None else length_ld
+        channel_width = self.ch_width if width is None else width
+        channel_length = self.ch_length if length is None else length
+        cgd = ((channel_width * l_ov) * c_ox) if c_gd is None else c_gd
+
+        c_gs = ((2 / 3) * (channel_width / channel_length) * c_ox) + cgd
+
+        return c_gs
+
+    def gate_drain_capacitance(self,
+                               c_ox: float,
+                               width: float=None,
+                               length_ld: float=None)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        c_ox : float
+            _description_
+        width : float, optional
+            _description_, by default None
+        length_ld : float, optional
+            _description_, by default None
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        l_ov = self.lat_diff_len if length_ld is None else length_ld
+        channel_width = self.ch_width if width is None else width
+
+        c_gd = (channel_width * l_ov) * c_ox
+
+        return c_gd
+
+    def source_body_capacitance(self,
+                                v_sb: float,
+                                v_o: float,
+                                c_sbo: float=None)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        c_sbo : float
+            _description_
+        v_sb : float
+            _description_
+        v_o : float
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        csbo = self.bulk_source_cap if c_sbo is None else c_sbo
+
+        c_sb = csbo / np.sqrt(1 + (v_sb / v_o))
+        return c_sb
+
+    def drain_body_capacitance(self,
+                               v_db: float,
+                               v_o: float,
+                               c_dbo: float=None)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        v_db : float
+            _description_
+        v_o : float
+            _description_
+        c_dbo : float, optional
+            _description_, by default None
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        cdbo = self.bulk_drain_cap if c_dbo is None else c_dbo
+
+        c_db = cdbo / np.sqrt(1 + (v_db / v_o))
+        return c_db
+
+    def transition_frequency(self,
+                             g_m: float,
+                             c_gs: float,
+                             c_gd: float)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        g_m : float
+            _description_
+        c_gs : float
+            _description_
+        c_gd : float
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        f_T = g_m / (2 * np.pi * (c_gs + c_gd))
+        return f_T
+
 class PMOS(MOSFET):
     """PMOS class"""
-    pass
+    def body_effect_parameter(self,
+                              concentration: float,
+                              c_ox: float)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        mos_type : str
+            Type of MOSFET. pMOS or nMOS. Input options are 'p' or 'n' 
+        concentration : float
+            _description_
+        c_ox : float
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        ELEMENTARY_CHARGE = value("Elementary charge")
+        gamma = -1.0*np.sqrt(2*ELEMENTARY_CHARGE*concentration) / c_ox
+        return gamma
+    
+    def operation_mode(self,
+                       v_gs: float,
+                       v_ds: float,
+                       v_tp: float)-> str:
+        """_summary_
+
+        Parameters
+        ----------
+        v_gs : float
+            _description_
+        v_ds : float
+            _description_
+        v_tp : float
+            _description_
+
+        Returns
+        -------
+        str
+            _description_
+        """
+        modes = ["Cut off", "Linear", "Saturation", "Lin/Sat Boundary"]
+
+        volt = v_gs - v_tp
+
+        if v_gs > v_tp:
+            return modes[0] #Cut off mode
+        elif (v_gs < v_tp) and (v_ds < volt):
+            return modes[1]
+        elif (v_gs < v_tp) and (v_ds > volt):
+            return modes[2]
+        elif (v_ds == volt):
+            return modes[3]
+
+    def drain_current(self,
+                      mode: str,
+                      current_gain: float,
+                      v_gs: float,
+                      v_tp: float,
+                      v_ds: float,
+                      lamda: float = 0.0)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        mode : str
+            _description_
+        current_gain : float
+            _description_
+        v_gs : float
+            _description_
+        v_tp : float
+            _description_
+        v_ds : float
+            _description_
+        lamda : float, optional
+            _description_, by default 0.0
+
+        Returns
+        -------
+        float
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
+        modes = ["Cut off", "Linear", "Saturation"]
+
+        if mode not in modes:
+            raise ValueError("Invalid mode! Mode must be Cut off, Linear, or Saturation!")
+
+        if mode == "Cut off":
+            return 0.0  #cutoff region drain current is always 0.0 Amperes
+        elif mode == "Linear":
+            if np.abs(v_ds) < 1.0:
+                return current_gain * (v_gs-v_tp) * v_ds #approx Linear drain current
+            else:
+                v_sq = (v_ds * (v_gs - v_tp)) - ((v_ds ** 2) / 2)
+                return current_gain * v_sq #triode region drain current
+        elif mode == "Saturation":
+            k = 0.5*current_gain
+            voltage_product = ((v_gs - v_tp) ** 2) * (1 + (lamda*v_ds))
+            return k * voltage_product #saturation region drain current
 
 class NMOS(MOSFET):
     """NMOS class"""
-    pass
+    def body_effect_parameter(self,
+                              concentration: float,
+                              c_ox: float)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        concentration : float
+            _description_
+        c_ox : float
+            _description_
+
+        Returns
+        -------
+        float
+            _description_
+        """
+        ELEMENTARY_CHARGE = value("Elementary charge")
+        gamma = np.sqrt(2*ELEMENTARY_CHARGE*concentration) / c_ox
+        return gamma
+
+    def operation_mode(self,
+                       v_gs: float,
+                       v_ds: float,
+                       v_tn: float)-> str:
+        """_summary_
+
+        Parameters
+        ----------
+        v_gs : float
+            _description_
+        v_ds : float
+            _description_
+        v_tn : float
+            _description_
+
+        Returns
+        -------
+        str
+            _description_
+        """
+        modes = ["Cut off", "Linear", "Saturation", "Lin/Sat Boundary"]
+
+        volt = v_gs - v_tn
+
+        if v_gs < v_tn:
+            return modes[0] #Cut off mode
+        elif (v_gs > v_tn) and (v_ds > volt):
+            return modes[1]
+        elif (v_gs > v_tn) and (v_ds < volt):
+            return modes[2]
+        elif (v_ds == volt):
+            return modes[3]
+
+    def drain_current(self,
+                      mode: str,
+                      current_gain: float,
+                      v_gs: float,
+                      v_tn: float,
+                      v_ds: float,
+                      lamda: float = 0.0)-> float:
+        """_summary_
+
+        Parameters
+        ----------
+        mode : str
+            _description_
+        current_gain : float
+            _description_
+        v_gs : float
+            _description_
+        v_tn : float
+            _description_
+        v_ds : float
+            _description_
+        lamda : float, optional
+            _description_, by default 0.0
+
+        Returns
+        -------
+        float
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
+        modes = ["Cut off", "Linear", "Saturation"]
+
+        if mode not in modes:
+            raise ValueError("Invalid mode! Mode must be Cut off, Linear, or Saturation!")
+
+        if mode == "Cut off":
+            return 0.0  #cutoff region drain current is always 0.0 Amperes
+        elif mode == "Linear":
+            if v_ds < 1.0:
+                return current_gain * (v_gs-v_tn) * v_ds #approx Linear drain current
+            else:
+                v_sq = (v_ds * (v_gs - v_tn)) - ((v_ds ** 2) / 2)
+                return current_gain * v_sq #triode region drain current
+        elif mode == "Saturation":
+            k = 0.5*current_gain
+            voltage_product = ((v_gs - v_tn) ** 2) * (1 + (lamda*v_ds))
+            return k * voltage_product #saturation region drain current
