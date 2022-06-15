05e2e4beeab2aef79c729dad218a0c4b5dcdbb77
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2022-06-15 06:22:07 -0500

    created BJT class

---
+++ semic/bjt/bjt.py
@@ -0,0 +1,14 @@
+"""BJT Class Module"""
+
+class BJT:
+    """BJT Class
+    """
+    def __init__(self,
+                 var1,
+                 var2,
+                 var3,
+                 varN) -> None:
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+        self.varN = varN
