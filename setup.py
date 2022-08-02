f2b5e5beeaaf901c82dfc9c3b05078fabbaacab2
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2022-08-01 23:01:39 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'semic',
+    packages = ['semic'],
+    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A package for semiconductor physics, calculations, and modeling',
+    author = 'Nithin Kumar Santha Kumar',
+    author_email = 'nxs169230@utdallas.edu',
+    url = 'https://github.com/nkskumar/semic',
+    download_url = 'https://github.com/nkskumar/semic/archive/refs/tags/v0.0.1.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
