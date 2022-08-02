bde6bf231f18a1b5bf21934de88651e2f1889c89
Nithin Kumar Santha Kumar <nxs169230@utdallas.edu>
2022-08-02 12:54:46 -0500

    started FinFET class

---
+++ semicpy/fets/finfet.py
@@ -0,0 +1,15 @@
+import numpy as np
+from semicpy.constants.constants import value
+
+class FinFET:
+    """FinFET Class
+    """
+    def __init__(self):
+        pass
+
+    def l_eff(self):
+        pass
+
+    def w_eff(self):
+        pass
+    
